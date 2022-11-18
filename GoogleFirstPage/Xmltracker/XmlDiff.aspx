53b728230b4ce37b9d2883bbf814cc499fd01e50
BVR <venkat@intelligentpositioning.com>
2022-11-18 23:26:27 +0530

    Added XmlDiff.aspx page to Xmltracker.

---
+++ GoogleFirstPage/Xmltracker/XmlDiff.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="XmlDiff.aspx.cs" Inherits="GoogleFirstPage.Xmltracker.XmlDiff" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Elementary by Pi</title>
+    <link href="css/main1.css" rel="stylesheet" />
+
+    <link rel="icon" href="images/default-avatar-logo.png" type="image/x-icon" />
+
+    <style type="text/css">
+        .text1 {}
+        .auto-style1 {
+            height: 56px;
+        }
+        .auto-style2 {
+            height: 187px;
+            width: 950px;
+        }
+        .auto-style3 {
+            font-size: 15px;
+            border-radius: 4px;
+            display: inline-block;
+            font-family: Calibri;
+            font-weight: bold;
+            font-style: normal;
+            color: #FFFFFF;
+            border: 1px solid #1583c2;
+            margin-left: 15px;
+            background: #1583c2;
+        }
+    </style>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="scriptmgr" runat="server" EnablePageMethods="false"></asp:ScriptManager>
+        <div class="header" style="width:4000px;">
+            <div class="header-menu">
+                <div style="margin-left: -80px;">
+                    <img src="images/pilogo.jpg" alt="pi-datametrics" class="imagedimensions" height="51" width="54" />
+                    <label class="labeltext1">Elementary by Pi</label>
+                </div>
+            </div>
+        </div>
+        <div class="displaydata">
+            <div style="margin-top: 1px; background-color: #e4e8ef; border-radius: 10px 10px; " class="auto-style2">
+                <br />
+                <div>
+                    <table style="margin-left: auto; margin-right: auto;">
+                        <tr>
+                            <td class="auto-style1">
+                                <strong><span>Select Date :</span></strong>
+                            </td>
+                            <td class="auto-style1">
+                                <asp:TextBox ID="txtDate" runat="server" placeholder="Date" Height="30px" Width="152px" Font-Size="Large" CssClass="text1"></asp:TextBox>
+                                <asp:ImageButton runat="Server" ID="DateImage" ImageUrl="~/images/calendarnew.png" Height="30px" Width="40px" />
+                                <ajaxToolkit:CalendarExtender ID="cDate" runat="server" PopupButtonID="DateImage" TargetControlID="txtDate" Format="yyyy-MM-dd" StartDate="2022-11-02" />
+                            </td>
+                            </tr>
+                        <tr>
+                            <td>
+                                <strong><span>UId :</span></strong>
+                            </td>
+                            <td>
+                                <asp:TextBox ID="txtUId" runat="server" placeholder="UId" Height="30px" Width="153px" Font-Size="Large" CssClass="text1"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <br />
+                                <asp:Button ID="btndatewise" runat="server" OnClick="btndatewise_Click" CssClass="auto-style3" Height="27px" Width="142px" Text="Submit" />
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+        </div>        
+       
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <div ID="XmlData" runat="server"></div>
+
+        <div class="footer">
+            &copy; <%= DateTime.Now.Year %> Pi Datametrics | XML Tracker.
+        </div>
+    </form>
+</body>
+</html>
---
+++ GoogleFirstPage/Xmltracker/XmlDiff.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace GoogleFirstPage.Xmltracker
+{
+    public partial class XmlDiff : System.Web.UI.Page
+    {
+        string connection = ConfigurationManager.ConnectionStrings["xmltracker"].ToString();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                txtDate.Attributes.Add("ReadOnly", "ReadOnly");
+                txtDate.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            }
+        }
+
+        protected void btndatewise_Click(object sender, EventArgs e)
+        {
+
+            string dt = txtDate.Text;
+            string uid = txtUId.Text;
+
+
+            string strQuery = "Exec [XmlTracker].[dbo].[GetXmlSourceChangeDate] " + uid + ", '" + dt + "'";
+            
+
+            int x = 0;
+            try
+            {
+                string curXml = string.Empty;
+                var prevXml = string.Empty;
+
+                using (SqlConnection con = new SqlConnection(connection))
+                {
+                    con.Open();
+                    SqlCommand comm = new SqlCommand(strQuery, con);
+                    comm.CommandTimeout = 0;
+                    SqlDataReader dr = comm.ExecuteReader();
+                    if(dr.Read())
+                    {
+                        curXml = dr.GetSqlXml(0).Value;
+                        prevXml = dr.GetSqlXml(1).Value;                        
+                    }
+                    dr.Close();
+                    comm.Dispose();
+                    con.Close();
+                }
+
+                string diffXml = XmlChanges(curXml, prevXml);
+
+                Response.Clear();
+                Response.ContentType = "text/xml";
+                Response.Write(diffXml);
+                Response.End();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }            
+
+        }
+
+        private string XmlChanges(string curXml, string prevXml)
+        {
+            XmlDocument curXmlDoc = new XmlDocument();
+            XmlDocument prevXmlDoc = new XmlDocument();
+
+            curXmlDoc.LoadXml(curXml);
+            prevXmlDoc.LoadXml(prevXml);
+
+            string url = curXmlDoc.SelectSingleNode("XmlSource").Attributes["url"].Value;
+            string curDate = curXmlDoc.SelectSingleNode("XmlSource").Attributes["date"].Value;
+            string prevDate = prevXmlDoc.SelectSingleNode("XmlSource").Attributes["date"].Value;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            sb.Append($"<XmlSource InDate=\"{curDate}\" OutDate=\"{prevDate}\" url=\"{url}\">");
+
+            XmlNodeList curList = curXmlDoc.SelectNodes("XmlSource/tag");
+            XmlNodeList prevList = prevXmlDoc.SelectNodes("XmlSource/tag");
+
+            foreach (XmlNode curNode in curList)
+            {
+                XmlNode cNode = curNode.ChildNodes[0];
+                if (cNode == null)
+                    continue;
+                XmlNodeList cNodes = cNode.ChildNodes;
+                string strChanges = string.Empty;
+                foreach (XmlNode prevNode in prevList)
+                {
+                    XmlNode pNode = prevNode.ChildNodes[0];
+                    if (pNode != null && pNode.Name == cNode.Name)
+                    {
+                        XmlNodeList pNodes = pNode.ChildNodes;
+                        string changes = GetChanges(cNodes, pNodes);
+                        if (!string.IsNullOrEmpty(changes))
+                        {
+                            strChanges += $"<{cNode.Name}>";
+                            strChanges += changes;
+                            strChanges += $"</{cNode.Name}>";
+                        }
+                        break;
+                    }
+                }
+                if (!string.IsNullOrEmpty(strChanges))
+                {
+                    sb.Append($"<tag type=\"{curNode.Attributes["type"].Value}\">");
+                    sb.Append(strChanges);
+                    sb.Append("</tag>");
+                }
+            }
+
+            sb.Append("</XmlSource>");
+            return sb.ToString();
+        }
+
+        private string GetChanges(XmlNodeList cNodes, XmlNodeList pNodes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int curCount = cNodes.Count;
+            int prevCount = pNodes.Count;
+
+            for (int i = 0; i < curCount; i++)
+            {
+                try
+                {
+                    if (cNodes.Item(i).OuterXml != pNodes.Item(i).OuterXml)
+                    {
+                        sb.Append($"<in>{cNodes.Item(i).OuterXml}</in>");
+                        sb.Append($"<out>{pNodes.Item(i).OuterXml}</out>");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex.GetType() == typeof(NullReferenceException))
+                    {
+                        sb.Append($"<in>{cNodes.Item(i).OuterXml}</in>");
+                        sb.Append("<out></out>");
+                    }
+                }
+            }
+
+            if (prevCount > curCount)
+                for (int i = curCount; i < prevCount; i++)
+                {
+                    sb.Append("<in></in>");
+                    sb.Append($"<out>{pNodes.Item(i).OuterXml}</out>");
+                }
+
+            return sb.ToString();
+        }
+    }
+}
