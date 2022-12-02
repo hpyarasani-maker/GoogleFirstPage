574d5b0a96e05245fd6f0857d4077140c4d57030
BVR <venkat@intelligentpositioning.com>
2022-12-02 20:56:31 +0530

    Updated Xml changes.

--- GoogleFirstPage/Xmltracker/MyCls.cs
+++ GoogleFirstPage/Xmltracker/MyCls.cs
@@ -8,10 +8,10 @@
         public bool blChng;
         public bool blColChng;
 
-        public string[] diffInfo(IEnumerator n, IEnumerator ol)
+        public string[] diffInfo(IEnumerator n, IEnumerator ol, string s)
         {
             string[] strArray = new string[] { "", "" };
-            Label_0154:
+        Label_0154:
             if (!n.MoveNext())
             {
                 ol.Reset();
@@ -29,8 +29,8 @@
                         {
                             string[] strArray2;
                             string[] strArray3;
-                            (strArray2 = strArray)[0] = strArray2[0] + "<li>" + setDiff(n.Current.ToString().Replace("<", "&lt;"), ol.Current.ToString().Replace("<", "&lt;"), "green") + "</li>";
-                            (strArray3 = strArray)[1] = strArray3[1] + "<li>" + setDiff(ol.Current.ToString().Replace("<", "&lt;"), n.Current.ToString().Replace("<", "&lt;"), "red") + "</li>";
+                            (strArray2 = strArray)[0] = strArray2[0] + "<li>" + setDiff(n.Current.ToString().Replace("<", "&lt;"), ol.Current.ToString().Replace("<", "&lt;"), "green", s) + "</li>";
+                            (strArray3 = strArray)[1] = strArray3[1] + "<li>" + setDiff(ol.Current.ToString().Replace("<", "&lt;"), n.Current.ToString().Replace("<", "&lt;"), "red", s) + "</li>";
                             this.blChng = true;
                             this.blColChng = true;
                         }
@@ -41,7 +41,7 @@
                         throw new Exception("at diff1 - " + exception.Message);
                     }
                 }
-                (strArray4 = strArray)[0] = strArray4[0] + "<li>" + setDiff(n.Current.ToString().Replace("<", "&lt;"), string.Empty, "green") + "</li>";
+                (strArray4 = strArray)[0] = strArray4[0] + "<li>" + setDiff(n.Current.ToString().Replace("<", "&lt;"), string.Empty, "green", s) + "</li>";
                 (strArray5 = strArray)[1] = strArray5[1] + "<li> - - - ";
                 this.blChng = true;
                 this.blColChng = true;
@@ -51,11 +51,16 @@
                 throw new Exception("at diff3 - " + exception2.Message);
             }
             goto Label_0154;
-            return strArray;
+            //return strArray;
         }
 
-        private string setDiff(string s1, string s2, string color)
+        private string setDiff(string s1, string s2, string color, string s)
         {
+            if (s == "A Href" || s == "Image Src")
+            {
+                return setUrlDiff(s1, s2, color);
+            }
+
             var newStr = s1.Split(' ');
             var oldStr = s2.Split(' ');
 
@@ -75,6 +80,31 @@
             return string.Join(" ", newStr);
         }
 
+        private string setUrlDiff(string s1, string s2, string color)
+        {
+            var newChr = s1.ToCharArray();
+            var oldChr = s2.ToCharArray();
+
+            var diffStr = string.Empty;
+
+            for (int i = 0; i < newChr.Length; i++)
+            {
+                try
+                {
+                    if (!newChr[i].Equals(oldChr[i]))
+                        diffStr += "<span style=\"color:" + color + "\">" + newChr[i] + "</span>";
+                    else
+                        diffStr += newChr[i];
+                }
+                catch
+                {
+                    diffStr += "<span style=\"color:" + color + "\">" + newChr[i] + "</span>";
+                }
+            }
+
+            return diffStr;
+        }
+
         public string getColumn(IEnumerator nd)
         {
 
@@ -87,4 +117,3 @@
         }
     }
 }
-
