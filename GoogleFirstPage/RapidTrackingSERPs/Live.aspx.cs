a0fa3444d7b245f7300f1fbfe513db4f5aad3680
Prakash <prakash@intelligentpositioning.com>
2024-04-23 11:25:01 +0100

    updated

--- GoogleFirstPage/RapidTrackingSERPs/Live.aspx.cs
+++ GoogleFirstPage/RapidTrackingSERPs/Live.aspx.cs
@@ -272,7 +272,15 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                                                 foreach (XmlNode nodee in node)
                                                 {
                                                     //dt.Rows.Add("", nodee.Attributes[1].Value, " featureTitle = " + nodee.Attributes[0].Value + " --- " + " Title = " + nodee.Attributes[2].Value + " --- " + " description = " + nodee.Attributes[3].Value + " --- " + " isTable = " + nodee.Attributes[4].Value + " --- " + " isList = " + nodee.Attributes[5].Value + " --- " + " isChart = " + nodee.Attributes[6].Value + " --- " + " isVideo = " + nodee.Attributes[7].Value); //+ " --- " + " Table = " + nodee.LastChild.Attributes[8].Value
-                                                    dt.Rows.Add("", node.Attributes[1].Value, " featureTitle = " + nodee.Attributes[0].Value + " --- " + " Title = " + nodee.Attributes[2].Value + " --- " + " description = " + nodee.Attributes[3].Value + " --- " + " cardType = " + nodee.Attributes[4].Value); //+ " --- " + " Table = " + nodee.LastChild.Attributes[8].Value
+                                                    //dt.Rows.Add("", node.Attributes[1].Value, " featureTitle = " + nodee.Attributes[0].Value + " --- " + " Title = " + nodee.Attributes[2].Value + " --- " + " description = " + nodee.Attributes[3].Value + " --- " + " cardType = " + nodee.Attributes[4].Value); //+ " --- " + " Table = " + nodee.LastChild.Attributes[8].Value
+                                                    if (node.Attributes[1].Value != null && node.Attributes[1].Value.ToString() != "")
+                                                    {
+                                                        dt.Rows.Add("", node.Attributes[1].Value, " featureTitle = " + nodee.Attributes[0].Value + " --- " + " Title = " + nodee.Attributes[2].Value + " --- " + " description = " + nodee.Attributes[3].Value + " --- " + " cardType = " + nodee.Attributes[4].Value); //+ " --- " + " Table = " + nodee.LastChild.Attributes[8].Value
+                                                    }
+                                                    if (node.Attributes[1].Value == null || node.Attributes[1].Value.ToString() == "")
+                                                    {
+                                                        dt.Rows.Add("", nodee.Attributes[1].Value, " featureTitle = " + nodee.Attributes[0].Value + " --- " + " Title = " + nodee.Attributes[2].Value + " --- " + " description = " + nodee.Attributes[3].Value + " --- " + " cardType = " + nodee.Attributes[4].Value); //+ " --- " + " Table = " + nodee.LastChild.Attributes[8].Value
+                                                    }
                                                 }
                                                 dt.Rows.Add("</" + node.Attributes[0].Value.ToString() + ">", "", "");
                                             }
