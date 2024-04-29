64fad944e2b3217721f35f3c190eeae4c5e14009
Prakash <prakash@intelligentpositioning.com>
2024-04-29 16:34:38 +0100

    updated ios and desktop class files

--- GoogleFirstPage/RapidTrackingSERPs/Desktop.cs
+++ GoogleFirstPage/RapidTrackingSERPs/Desktop.cs
@@ -491,7 +491,7 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                 if (carousel != null)//07-12-2023 end
                 {
                     s.Append("<block type=\"carousel\" url=\"\">");
-                    //s.Append(GetCarousel(crNode));  // 23-10-2019
+                    s.Append(GetCarousel(carousel));//29-04-2024 uncommented  // 23-10-2019
                     s.Append("</block>");
                 }
             }
@@ -2511,7 +2511,10 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                     {
                         indx = url.LastIndexOf("https://");
                     }
-                    url = url.Remove(0, indx);
+                    if (indx > 0) //29-04-2024
+                    {
+                        url = url.Remove(0, indx);
+                    }//29-04-2024
                 }//28-03-2024
 
                 if (url.Contains("&amp;grqid="))
