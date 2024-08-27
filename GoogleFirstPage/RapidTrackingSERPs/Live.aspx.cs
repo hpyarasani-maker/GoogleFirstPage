8a3d676b37080a1640f83705aaa10760199e01ea
Prakash <prakash@intelligentpositioning.com>
2024-08-27 13:01:39 +0100

    updated

--- GoogleFirstPage/RapidTrackingSERPs/Live.aspx.cs
+++ GoogleFirstPage/RapidTrackingSERPs/Live.aspx.cs
@@ -360,7 +360,8 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                                                 }
                                                 foreach (XmlNode nodee in node)
                                                 {
-                                                    dt.Rows.Add("", "", " url= " + "" + " , " + " airline= " + node.Attributes[1].Value + "  ,  " + " title= " + nodee.Attributes[2].Value + "  ,  " + " duration= " + nodee.Attributes[3].Value + " , " + " durationValue= " + nodee.Attributes[4].Value + " , " + " connections= " + nodee.Attributes[5].Value + " , " + " price= " + nodee.Attributes[6].Value + " , " + " priceValue= " + nodee.Attributes[7].Value);
+                                                    //dt.Rows.Add("", "", " url= " + "" + " , " + " airline= " + node.Attributes[1].Value + "  ,  " + " title= " + nodee.Attributes[2].Value + "  ,  " + " duration= " + nodee.Attributes[3].Value + " , " + " durationValue= " + nodee.Attributes[4].Value + " , " + " connections= " + nodee.Attributes[5].Value + " , " + " price= " + nodee.Attributes[6].Value + " , " + " priceValue= " + nodee.Attributes[7].Value);
+                                                    dt.Rows.Add("", "", " url= " + "" + " , " + " title= " + nodee.Attributes[2].Value + "  ,  " + " airline= " + nodee.Attributes[1].Value + "  ,  " + " duration= " + nodee.Attributes[3].Value + " , " + " durationValue= " + nodee.Attributes[4].Value + " , " + " connections= " + nodee.Attributes[5].Value + " , " + " price= " + nodee.Attributes[6].Value + " , " + " priceValue= " + nodee.Attributes[7].Value);
                                                 }
                                                 dt.Rows.Add("</" + node.Attributes[0].Value.ToString() + ">", "", "");
                                             }
