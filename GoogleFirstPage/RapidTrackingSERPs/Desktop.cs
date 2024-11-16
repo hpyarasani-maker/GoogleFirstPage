88f71026eaa9d0daa890cc33f70f53ea6bfbb207
Prakashboora <51644132+Prakashboora@users.noreply.github.com>
2024-11-16 17:26:44 +0000

    updated

--- GoogleFirstPage/RapidTrackingSERPs/Desktop.cs
+++ GoogleFirstPage/RapidTrackingSERPs/Desktop.cs
@@ -2630,7 +2630,7 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                             }
                             if (!string.IsNullOrEmpty(SetUrl(url)) || !string.IsNullOrEmpty(content))
                             {
-                                s.Append("<item url=\"" + SetUrl(url).Replace("&nbsp;", "") + "\" content=\"" + content.Replace("&nbsp;", "") + "\" />");
+                                s.Append("<item url=\"" + SetUrl(url).Replace("&nbsp;", "") + "\" content=\"" + SetTitle(content.Replace("&nbsp;", "")) + "\" />");//16-11-2024
                             }
                         }
                     }
