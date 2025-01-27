684d0b43662b85dc1a71fe52ac8fed446ad03b36
Prakashboora <51644132+Prakashboora@users.noreply.github.com>
2025-01-27 19:00:31 +0000

    updated ios and desktop class files

--- GoogleFirstPage/RapidTrackingSERPs/Desktop.cs
+++ GoogleFirstPage/RapidTrackingSERPs/Desktop.cs
@@ -2821,6 +2821,12 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                 if (url.Contains("&gclid="))
                     url = url.Remove(url.IndexOf("&gclid="));
                 //end 23-09-2020
+                if (url.Contains("%3Fsrsltid="))//27-01-2025
+                    url = url.Remove(url.IndexOf("%3Fsrsltid"));
+                if (url.Contains("?srsltid="))
+                    url = url.Remove(url.IndexOf("?srsltid="));
+                if (url.Contains("&srsltid="))
+                    url = url.Remove(url.IndexOf("&srsltid="));//27-01-2025
                 if (url.Contains("&amp;sa="))//27-01-2025
                     url = url.Remove(url.IndexOf("&amp;sa="));
                 if (url.Contains("&sa="))
@@ -2954,6 +2960,12 @@ namespace GoogleFirstPage.RapidTrackingSERPs
                 //end 23-09-2020
                 if (url.Contains("\0"))
                     url = url.Replace("\0", "%00");
+                if (url.Contains("%3Fsrsltid="))//27-01-2025
+                    url = url.Remove(url.IndexOf("%3Fsrsltid"));
+                if (url.Contains("?srsltid="))
+                    url = url.Remove(url.IndexOf("?srsltid="));
+                if (url.Contains("&srsltid="))
+                    url = url.Remove(url.IndexOf("&srsltid="));//27-01-2025
                 if (url.Contains("&amp;sa="))//27-01-2025
                     url = url.Remove(url.IndexOf("&amp;sa="));
                 if (url.Contains("&sa="))
